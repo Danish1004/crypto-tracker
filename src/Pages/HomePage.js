abede2a59a9c0e6cc7afbc917834efa411d36b1e
Danish Sharma <danishsharma13255@gmail.com>
2023-09-05 00:33:36 +0530

    Routing and components

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Banner from "../Components/banner/Banner";
+
+const HomePage = () => {
+  return (
+    <>
+      <Banner />
+    </>
+  );
+};
+
+export default HomePage;
